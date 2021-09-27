0af86aaf304f54c01c72b4618cb3a59045cf77da
exuberantram <exuberantram@gmail.com>
2021-09-27 17:03:21 -0400

    started JS

---
+++ rdt_script.js
@@ -0,0 +1,12 @@
+let mins = 0;
+let hours = 0;
+let days = 0;
+
+document.getElementById("mins").innerHTML = mins;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("days").innerHTML = days;
+
+const button = document.querySelector('input');
+button.addEventListener('click');
+
+var min1 = document.getElementById("1min").value;
