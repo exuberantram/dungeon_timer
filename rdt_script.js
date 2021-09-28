cea82b7e37ad952578225d9cd21a84291fbe2481
exuberantram <exuberantram@gmail.com>
2021-09-27 20:17:50 -0400

    created addTheValue and Reset function w/ rollover

--- rdt_script.js
+++ rdt_script.js
@@ -1,12 +1,42 @@
-let mins = 0;
-let hours = 0;
-let days = 0;
+let mins = document.getElementById('mins');
+let hours = document.getElementById('hours');
+let days = document.getElementById('days');
+// going to try addTheValue from https://www.tutorialspoint.com/javascript-sum-function-on-the-click-of-a-button for the sum
 
-document.getElementById("mins").innerHTML = mins;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("days").innerHTML = days;
+function addTheValueMin(secondValue) {
+    mins.innerHTML = parseInt(mins.innerHTML) + parseInt(secondValue);
+    if (mins.innerHTML >= 60) {
+        mins.innerHTML = mins.innerHTML-60;
+        hours.innerHTML++;
+        if (hours.innerHTML >= 24) {
+            hours.innerHTML = hours.innerHTML-24;
+            days.innerHTML++;
+        }
+        
+    }
+}
 
-const button = document.querySelector('input');
-button.addEventListener('click');
+function addTheValueHour(secondValue) {
+    hours.innerHTML = parseInt(hours.innerHTML) + parseInt(secondValue);
+    if (hours.innerHTML >= 24) {
+        hours.innerHTML = hours.innerHTML-24;
+        days.innerHTML++;
+    }
+}
 
-var min1 = document.getElementById("1min").value;
+function addTheValueDay(secondValue) {
+    days.innerHTML = parseInt(days.innerHTML) + parseInt(secondValue);
+}
+
+
+function resetTime() { // function to set everything back to 0
+    document.getElementById('mins').innerHTML = 0;
+    document.getElementById('hours').innerHTML = 0;
+    document.getElementById('days').innerHTML = 0;
+}
+
+
+
+// make the counter roll over to hours at 60 mins and days at 24 hours
+// after counter roll over, make reset button optionally save
+
